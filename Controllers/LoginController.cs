24bb04f14f6a74e511f0b87cbd8624e14d95f5a8
Paul Abib Camano <paul.camano@gmail.com>
2024-09-09 13:01:45 +0800

    add login POC

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,21 @@
+﻿using jsonapisample.Models.Integration;
+using jsonapisample.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace jsonapisample.Controllers
+{
+    public class LoginController(LoginService loginService) : ControllerBase
+    {
+        private readonly LoginService _loginService = loginService;
+
+        [HttpPost("/api/v{apiVersion}/users/login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequest request)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            bool result = await _loginService.Login(request);
+            return Ok(new { result });
+        }
+    }
+}
---
+++ Services/LoginService.cs
@@ -0,0 +1,24 @@
+﻿using jsonapisample.Data;
+using jsonapisample.Models;
+using jsonapisample.Models.Integration;
+using Microsoft.EntityFrameworkCore;
+
+namespace jsonapisample.Services
+{
+    public class LoginService(AppDbContext context)
+    {
+        private readonly AppDbContext _context = context;
+
+        public async Task<bool> Login(LoginRequest creds)
+        {
+            if (creds == null)
+                return false;
+
+            User? user = await _context.Users.Where(x => x.UserName == creds.Username).FirstOrDefaultAsync();
+            if (user == null)
+                return false;
+
+            return user.Password.Equals(creds.Password);
+        }
+    }
+}
